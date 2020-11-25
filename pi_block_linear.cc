0e5a6b18f006a3b942f691c72802635e9b68b38b
“yuhao” <tim00631@gmail.com>
2020-11-25 11:20:52 +0800

    change RNG

--- pi_block_linear.cc
+++ pi_block_linear.cc
@@ -19,16 +19,16 @@
 //     return w = w ^ (w >> 19) ^ t ^ (t >> 8);
 // }
 
-static uint64_t s[2];
+static u_int64_t s[2];
 
-static inline uint64_t rotl(const uint64_t x, int k) {
+static inline u_int64_t rotl(const u_int64_t x, int k) {
 	return (x << k) | (x >> (64 - k));
 }
 
-uint64_t next(void) {
-	const uint64_t s0 = s[0];
-	uint64_t s1 = s[1];
-	const uint64_t result = s0 + s1;
+u_int64_t next(void) {
+	const u_int64_t s0 = s[0];
+	u_int64_t s1 = s[1];
+	const u_int64_t result = s0 + s1;
 
 	s1 ^= s0;
 	s[0] = rotl(s0, 24) ^ s1 ^ (s1 << 16); // a, b
@@ -43,10 +43,10 @@ uint64_t next(void) {
    non-overlapping subsequences for parallel computations. */
 
 void jump(void) {
-	static const uint64_t JUMP[] = { 0xdf900294d8f554a5, 0x170865df4b3201fc };
+	static const u_int64_t JUMP[] = { 0xdf900294d8f554a5, 0x170865df4b3201fc };
 
-	uint64_t s0 = 0;
-	uint64_t s1 = 0;
+	u_int64_t s0 = 0;
+	u_int64_t s1 = 0;
 	for(int i = 0; i < sizeof JUMP / sizeof *JUMP; i++)
 		for(int b = 0; b < 64; b++) {
 			if (JUMP[i] & UINT64_C(1) << b) {
@@ -67,10 +67,10 @@ void jump(void) {
    subsequences for parallel distributed computations. */
 
 void long_jump(void) {
-	static const uint64_t LONG_JUMP[] = { 0xd2a98b26625eee7b, 0xdddf9b1090aa7ac1 };
+	static const u_int64_t LONG_JUMP[] = { 0xd2a98b26625eee7b, 0xdddf9b1090aa7ac1 };
 
-	uint64_t s0 = 0;
-	uint64_t s1 = 0;
+	u_int64_t s0 = 0;
+	u_int64_t s1 = 0;
 	for(int i = 0; i < sizeof LONG_JUMP / sizeof *LONG_JUMP; i++)
 		for(int b = 0; b < 64; b++) {
 			if (LONG_JUMP[i] & UINT64_C(1) << b) {
