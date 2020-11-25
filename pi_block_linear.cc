cd1a7d2b740f4f23b5b8f758fa3ddda2fb29c7ab
“yuhao” <tim00631@gmail.com>
2020-11-25 11:16:23 +0800

    change RNG

--- pi_block_linear.cc
+++ pi_block_linear.cc
@@ -5,28 +5,93 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-__uint32_t xor128(__uint32_t seed) {
-    static __uint32_t x = seed;
-    static __uint32_t y = 362436069;
-    static __uint32_t z = 521288629;
-    static __uint32_t w = 88675123;
-    __uint32_t t;
+// __uint32_t xor128(__uint32_t seed) {
+//     static __uint32_t x = seed;
+//     static __uint32_t y = 362436069;
+//     static __uint32_t z = 521288629;
+//     static __uint32_t w = 88675123;
+//     __uint32_t t;
 
-    t = x ^ (x << 11);
-    x = y;
-    y = z;
-    z = w;
-    return w = w ^ (w >> 19) ^ t ^ (t >> 8);
+//     t = x ^ (x << 11);
+//     x = y;
+//     y = z;
+//     z = w;
+//     return w = w ^ (w >> 19) ^ t ^ (t >> 8);
+// }
+
+static uint64_t s[2];
+
+static inline uint64_t rotl(const uint64_t x, int k) {
+	return (x << k) | (x >> (64 - k));
 }
 
-float fRand(__uint32_t seed) {
-    // long long MAX = ((long long)RAND_MAX << 31) + RAND_MAX;
-    // long long rand_num = ((long long)rand() << 31) + rand();
-    // printf("%lld, %lld, %lf\n", rand_num, MAX, (double)rand_num/MAX);
+uint64_t next(void) {
+	const uint64_t s0 = s[0];
+	uint64_t s1 = s[1];
+	const uint64_t result = s0 + s1;
 
-    return (float)xor128(seed) / __UINT32_MAX__;
-    // return ((double)rand_num / (double)MAX);
+	s1 ^= s0;
+	s[0] = rotl(s0, 24) ^ s1 ^ (s1 << 16); // a, b
+	s[1] = rotl(s1, 37); // c
+
+	return result;
 }
+
+
+/* This is the jump function for the generator. It is equivalent
+   to 2^64 calls to next(); it can be used to generate 2^64
+   non-overlapping subsequences for parallel computations. */
+
+void jump(void) {
+	static const uint64_t JUMP[] = { 0xdf900294d8f554a5, 0x170865df4b3201fc };
+
+	uint64_t s0 = 0;
+	uint64_t s1 = 0;
+	for(int i = 0; i < sizeof JUMP / sizeof *JUMP; i++)
+		for(int b = 0; b < 64; b++) {
+			if (JUMP[i] & UINT64_C(1) << b) {
+				s0 ^= s[0];
+				s1 ^= s[1];
+			}
+			next();
+		}
+
+	s[0] = s0;
+	s[1] = s1;
+}
+
+
+/* This is the long-jump function for the generator. It is equivalent to
+   2^96 calls to next(); it can be used to generate 2^32 starting points,
+   from each of which jump() will generate 2^32 non-overlapping
+   subsequences for parallel distributed computations. */
+
+void long_jump(void) {
+	static const uint64_t LONG_JUMP[] = { 0xd2a98b26625eee7b, 0xdddf9b1090aa7ac1 };
+
+	uint64_t s0 = 0;
+	uint64_t s1 = 0;
+	for(int i = 0; i < sizeof LONG_JUMP / sizeof *LONG_JUMP; i++)
+		for(int b = 0; b < 64; b++) {
+			if (LONG_JUMP[i] & UINT64_C(1) << b) {
+				s0 ^= s[0];
+				s1 ^= s[1];
+			}
+			next();
+		}
+
+	s[0] = s0;
+	s[1] = s1;
+}
+
+// float fRand(__uint32_t seed) {
+//     // long long MAX = ((long long)RAND_MAX << 31) + RAND_MAX;
+//     // long long rand_num = ((long long)rand() << 31) + rand();
+//     // printf("%lld, %lld, %lf\n", rand_num, MAX, (double)rand_num/MAX);
+
+//     return (float)xor128(seed) / __UINT32_MAX__;
+//     // return ((double)rand_num / (double)MAX);
+// }
 
 int main(int argc, char **argv)
 {
@@ -43,17 +108,19 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
     __uint32_t seed = time(NULL) * world_rank;
+    s[0] = seed;
+    s[1] = seed<<2;
     long long iteration = tosses / world_size; 
     long long int* local_count;
 
     if (world_rank > 0) {
         // TODO: handle workers
         long long int number_in_circle = 0;
-
         for (int i = 0; i < iteration; i++) {
-            float x = fRand(seed);
-            float y = fRand(seed);
-            float distance_squared = x * x + y * y;
+            long_jump();
+            double x = (double)s[0] / UINT64_MAX;
+            double y = (double)s[1] / UINT64_MAX;
+            double distance_squared = x * x + y * y;
             if (distance_squared <= 1) {
                 number_in_circle++;
             }
