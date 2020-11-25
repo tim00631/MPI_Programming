328ff00ed2618855621b1398a6cd104c7a258ff0
“yuhao” <tim00631@gmail.com>
2020-11-25 21:02:15 +0800

    block_linear RNG

--- pi_block_linear.cc
+++ pi_block_linear.cc
@@ -6,41 +6,41 @@
 #include <unistd.h>
 #include <stdint.h>
 
-// struct xorshift128p_state {
-//   uint64_t a, b;
-// };
+struct xorshift128p_state {
+  uint64_t a, b;
+};
 
-// /* The state must be seeded so that it is not all zero */
-// uint64_t xorshift128p(struct xorshift128p_state *state)
-// {
-// 	uint64_t t = state->a;
-// 	uint64_t const s = state->b;
-// 	state->a = s;
-// 	t ^= t << 23;		// a
-// 	t ^= t >> 17;		// b
-// 	t ^= s ^ (s >> 26);	// c
-// 	state->b = t;
-// 	return t + s;
+/* The state must be seeded so that it is not all zero */
+uint64_t xorshift128p(struct xorshift128p_state *state)
+{
+	uint64_t t = state->a;
+	uint64_t const s = state->b;
+	state->a = s;
+	t ^= t << 23;		// a
+	t ^= t >> 17;		// b
+	t ^= s ^ (s >> 26);	// c
+	state->b = t;
+	return t + s;
+}
+
+// static inline uint64_t rotl(const uint64_t x, int k) {
+// 	return (x << k) | (x >> (64 - k));
 // }
 
-static inline uint64_t rotl(const uint64_t x, int k) {
-	return (x << k) | (x >> (64 - k));
-}
 
+// static uint64_t s[2];
 
-static uint64_t s[2];
+// uint64_t next(void) {
+// 	const uint64_t s0 = s[0];
+// 	uint64_t s1 = s[1];
+// 	const uint64_t result = s0 + s1;
 
-uint64_t next(void) {
-	const uint64_t s0 = s[0];
-	uint64_t s1 = s[1];
-	const uint64_t result = s0 + s1;
+// 	s1 ^= s0;
+// 	s[0] = rotl(s0, 24) ^ s1 ^ (s1 << 16); // a, b
+// 	s[1] = rotl(s1, 37); // c
 
-	s1 ^= s0;
-	s[0] = rotl(s0, 24) ^ s1 ^ (s1 << 16); // a, b
-	s[1] = rotl(s1, 37); // c
-
-	return result;
-}
+// 	return result;
+// }
 
 int main(int argc, char **argv)
 {
@@ -57,11 +57,11 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     __uint32_t seed = time(NULL) * (world_rank + 1);
 
-    // struct xorshift128p_state* state = (struct xorshift128p_state*)malloc(sizeof(struct xorshift128p_state));
-    // state->a = seed + 1;
-    // state->b = seed & 0x55555555;
-    s[0] = seed;
-    s[1] = s[0] & 0x55555555;
+    struct xorshift128p_state* state = (struct xorshift128p_state*)malloc(sizeof(struct xorshift128p_state));
+    state->a = seed + 1;
+    state->b = seed & 0x55555555;
+    // s[0] = seed;
+    // s[1] = s[0] & 0x55555555;
     long long iteration = tosses / world_size; 
     long long int* local_count;
 
@@ -69,15 +69,15 @@ int main(int argc, char **argv)
         // TODO: handle workers
         long long int number_in_circle = 0;
         for (int i = 0; i < iteration; i++) {
-            // uint64_t tmp = xorshift128p(state);
-            // double x = (double)(tmp << 32 >> 32) / __UINT32_MAX__;
-            // double y = (double)(tmp >> 32) / __UINT32_MAX__;
+            uint64_t tmp = xorshift128p(state);
+            float x = (float)(tmp << 32 >> 32) / __UINT32_MAX__;
+            float y = (float)(tmp >> 32) / __UINT32_MAX__;
 
-            uint64_t tmp = next();
-            double x = (double) s[0] / __UINT64_MAX__;
-            double y = (double) s[1] / __UINT64_MAX__;
+            // uint64_t tmp = next();
+            // double x = (double) s[0] / __UINT64_MAX__;
+            // double y = (double) s[1] / __UINT64_MAX__;
 
-            double distance_squared = x * x + y * y;
+            float distance_squared = x * x + y * y;
             if (distance_squared <= 1) {
                 number_in_circle++;
             }
@@ -97,13 +97,13 @@ int main(int argc, char **argv)
         local_count =(long long int*)malloc(sizeof(long long int) * world_size); // initialize global variable
         long long int number_in_circle = 0;
         for (int i = 0; i < tosses / world_size; i++) {
-            // uint64_t tmp = xorshift128p(state);
-            // double x = (double)(tmp << 32 >> 32) / __UINT32_MAX__;
-            // double y = (double)(tmp >> 32) / __UINT32_MAX__;
+            uint64_t tmp = xorshift128p(state);
+            float x = (float)(tmp << 32 >> 32) / __UINT32_MAX__;
+            float y = (float)(tmp >> 32) / __UINT32_MAX__;
 
-            uint64_t tmp = next();
-            double x = (double) s[0] / __UINT64_MAX__;
-            double y = (double) s[1] / __UINT64_MAX__;
+            // uint64_t tmp = next();
+            // double x = (double) s[0] / __UINT64_MAX__;
+            // double y = (double) s[1] / __UINT64_MAX__;
             float distance_squared = x * x + y * y;
             if (distance_squared <= 1) {
                 number_in_circle++;
