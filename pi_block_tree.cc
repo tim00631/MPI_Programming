e71fc09a495a4c1dfcf6ab779b820cd829b917fd
“yuhao” <tim00631@gmail.com>
2020-11-27 19:09:29 +0800

    rs array look up table

--- pi_block_tree.cc
+++ pi_block_tree.cc
@@ -91,7 +91,7 @@ int main(int argc, char **argv)
         }
         else if(rs[world_rank][i] == -1) {
             MPI_Send(
-            /* data         = */ number_in_circle, 
+            /* data         = */ &number_in_circle, 
             /* count        = */ 1, 
             /* datatype     = */ MPI_LONG_LONG, 
             /* source       = */ world_rank - s, 
