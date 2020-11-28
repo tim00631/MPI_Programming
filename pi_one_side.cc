6ed7cb89f59b73d83b6c6aa91ca0c3dc896f62f2
“yuhao” <tim00631@gmail.com>
2020-11-28 16:44:45 +0800

    one-side

--- pi_one_side.cc
+++ pi_one_side.cc
@@ -63,17 +63,19 @@ int main(int argc, char **argv)
         // Master
         local_count = (uint64_t *) malloc(sizeof(uint64_t) * world_size);
         MPI_Win_create(local_count, sizeof(uint64_t), world_size, MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+        MPI_Win_lock(MPI_LOCK_EXCLUSIVE, 0, 0, win);
         MPI_Put(&number_in_circle, 1, MPI_LONG_LONG, 0, world_rank, 1, MPI_LONG_LONG, win);
+        MPI_Win_unlock(0, win);
     }
     else
     {
         // Workers
         MPI_Win_create(local_count, 0, 1, MPI_INFO_NULL, MPI_COMM_WORLD, &win);
-        // MPI_Win_lock(MPI_LOCK_EXCLUSIVE, 0, 0, win);
         // MPI_Get(&total_count, 1, MPI_LONG_LONG, 0, 0, 1, MPI_LONG_LONG, win);
         // total_count += number_in_circle;
+        MPI_Win_lock(MPI_LOCK_EXCLUSIVE, 0, 0, win);
         MPI_Put(&number_in_circle, 1, MPI_LONG_LONG, 0, world_rank, 1, MPI_LONG_LONG, win);
-        // MPI_Win_unlock(0, win);
+        MPI_Win_unlock(0, win);
         
     }
 
